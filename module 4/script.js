ddbde3a123b47fb24ef815d868d4b44334941aa6
swamy1199 <54707966+swamy1199@users.noreply.github.com>
2020-06-27 10:16:27 +0530

    Update script.js

--- module 4/script.js
+++ module 4/script.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 (function() {
-    var names = ["Godha", "Venkata", "Swamy", "Fayaz", "Kumar", "Anoop", "Ram", "Kamal", "John", "Victor"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'S') {
